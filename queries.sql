b13a03477e6e2e12c769d7ef358e1e53ea839e9c
igurianova <95527497+igurianova@users.noreply.github.com>
2022-12-01 14:41:22 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -52,3 +52,46 @@ FROM book_review br
 GROUP BY br.id, u.id, b.id, a.id
 ORDER BY likes DESC
 LIMIT 10;
+
+-- Найти всех пользователей, которые зарегистрировались за последние 3 месяца
+SELECT *
+FROM users u
+WHERE u.reg_time + interval '3 month' >= CURRENT_DATE;
+
+-- Пользователь, который оставил больше всего отзывов?
+SELECT u.id, name, count(br.id) as count
+FROM book_review br
+         INNER JOIN users u on u.id = br.user_id
+GROUP BY u.id
+ORDER BY count DESC
+LIMIT 1;
+
+-- В какой день января оставили больше всего отзывов?
+SELECT DATE_PART('day', br.time) as day, count(br.id) as count
+FROM book_review br
+WHERE DATE_PART('month', br.time) = '1'
+GROUP BY day
+ORDER BY count DESC
+LIMIT 1;
+
+-- Топ 7 бестселлеров за текущий год?
+SELECT b.id,
+       b.title,
+       b.is_bestseller,
+       count(b2u.user_id) as buy_count
+FROM books b
+         INNER JOIN book2user b2u on b.id = b2u.book_id
+WHERE b.is_bestseller = 1
+  AND DATE_PART('year', b2u.time) = DATE_PART('year', CURRENT_DATE)
+GROUP BY b.id
+ORDER BY buy_count DESC
+LIMIT 7;
+
+-- Топ 5 пользователей, которые потратили больше всего денег при покупке книг
+SELECT u.id, u.name, sum(b.price) as total_amount
+FROM book2user b2u
+         INNER JOIN users u on u.id = b2u.user_id
+         INNER JOIN books b on b.id = b2u.book_id
+GROUP BY u.id
+ORDER BY total_amount DESC
+LIMIT 5;
